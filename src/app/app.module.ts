59c5662b613f8198592c6b7126e3f0cbe2f52c4d
Jigar Wala <jigarwala007@gmail.com>
2019-07-11 16:11:08 +0530

    integrate with todobackend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from "@angular/common/http"
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
